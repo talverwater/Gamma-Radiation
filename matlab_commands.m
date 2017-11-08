3228f7685dd132730c414f1db1fcf0bcbdba177c
eitanba <33030167+eitanba@users.noreply.github.com>
2017-11-08 14:57:29 +0200

    adding linear fit and conversion between energy and chanel
    fit by Na-22 only 2 dots fit.

--- matlab_commands.m
+++ matlab_commands.m
@@ -1,3 +1,4 @@
+%% import data block
 names = {...
     'Cs-137_14min',...
     'background_1h_20m',...
@@ -9,3 +10,14 @@ for ind=1:length(names)
     dataArray(ind,:) = importrawdata(char(names(ind)));
 end
 clear ind
+
+%% finding chanel energy linear fit
+Na_22_picks_chanel = [6257,2500];
+Na_22_picks_energies_books = [1.274,0.511];
+
+% coef values of linear fit
+coefavl = polyfit(Na_22_picks_chanel,Na_22_picks_energies_books,1);
+% converting chanel to energy by the linear fit we foud
+x = 1:size(dataArray,2);
+Ene_MeV = coefavl(1)*x+coefavl(2);
+plot(Ene_MeV,dataArray(2,:));shg;
